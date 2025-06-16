4bb78137b9fa1330b9b44979921d290654c9a2db
LiquidApplesx <liquidapples@proton.me>
2025-06-15 20:38:23 -0400

    Updates to authentication

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -2,24 +2,20 @@
 {
   programs.ssh = {
     enable = true;
-
     addKeysToAgent = "1h";
-
     controlMaster = "auto";
     controlPath = "~/.ssh/control-%r@%h:%p";
     controlPersist = "10m";
-
     matchBlocks = {
       github = {
         host = "github.com";
         hostname = "ssh.github.com";
         user = "git";
         port = 443;
-        identityFile = "~/.ssh/id_ed25519";
+        identityFile = "~/.ssh/id_ed25519"; # Same filename on all machines
         identitiesOnly = true;
       };
     };
   };
-
   services.ssh-agent.enable = true;
 }
